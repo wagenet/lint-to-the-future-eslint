5f009c7d6b28a42bf4e455f25449e79ab8e8aea0
Chris Manson <chris@manson.ie>
2020-12-13 22:23:41 +0000

    port eslint implementation from lint-to-the-future 0.1

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-export function ignoreAll() {
+const { readFileSync, writeFileSync } = require('fs');
+const importCwd = require('import-cwd');
 
+function ignoreError(error) {
+  const ruleIds = error.messages.map(message => message.ruleId);
+  let uniqueIds = [...new Set(ruleIds)];
+
+  const file = readFileSync(error.filePath, 'utf8');
+
+  const firstLine = file.split('\n')[0];
+
+  if (firstLine.includes('eslint-disable')) {
+    console.warn('appending existing disables not supported yet');
+  } else {
+    writeFileSync(error.filePath, `/* eslint-disable ${uniqueIds.join(', ')} */\n${file}`);
+  }
 }
 
-export function ignore(name) {
+export function ignoreAll() {
+  const eslint = importCwd('eslint');
 
+  const { CLIEngine } = eslint;
+
+  const cli = new CLIEngine();
+
+  const report = cli.executeOnFiles(['.']);
+
+  const errors = report.results.filter(result => result.errorCount > 0);
+
+  errors.forEach(ignoreError);
 }
 
 export function list() {
-
+  throw new Error('listing not supported yet in this plugin');
 }
