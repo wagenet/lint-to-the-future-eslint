8eeb64b54c06832f1a5f7ea944e9bf6bce6eda09
Ricardo Mendes <rokusu@gmail.com>
2022-06-09 18:37:21 +0100

    ignore eslint-disable-next-line and variations

--- test/ignore.mjs
+++ test/ignore.mjs
@@ -56,4 +56,88 @@ debugger`);
     expect(result['test.js']).to.equal(`/* eslint-disable no-debugger */
 debugger`);
   });
+
+  it('should handle files with `// eslint-disable` comments', async function () {
+    this.timeout(20000);
+    const tempDir = await temp.mkdir('super-app');
+
+    fixturify.writeSync(tempDir, {
+      '.eslintrc.json': '{"extends": "eslint:recommended"}',
+      'test.js': `// eslint-disable no-debugger
+debugger`,
+      'package.json': `{
+  "devDependencies": {
+    "eslint": "^8.0.0"
+  }
+}
+`,
+    });
+
+    let result = await execa('npm', ['i'], { cwd: tempDir });
+
+    await ignoreAll(tempDir);
+
+    result = fixturify.readSync(tempDir);
+
+    expect(result['test.js']).to.equal(`/* eslint-disable no-debugger */
+debugger`);
+  });
+
+  it('should add to existing `/* eslint-disable` comments', async function () {
+    this.timeout(20000);
+    const tempDir = await temp.mkdir('super-app');
+
+    fixturify.writeSync(tempDir, {
+      '.eslintrc.json': '{"extends": "eslint:recommended"}',
+      'test.js': `/* eslint-disable no-console, no-undef */
+debugger
+console.log('test')`,
+      'package.json': `{
+  "devDependencies": {
+    "eslint": "^8.0.0"
+  }
+}
+`,
+    });
+
+    let result = await execa('npm', ['i'], { cwd: tempDir });
+
+    await ignoreAll(tempDir);
+
+    result = fixturify.readSync(tempDir);
+
+    expect(result['test.js']).to
+      .equal(`/* eslint-disable no-debugger, no-console, no-undef */
+debugger
+console.log('test')`);
+  });
+
+  it('handles `// eslint-disable-next-line` at the top of the file correctly', async function () {
+    this.timeout(20000);
+    const tempDir = await temp.mkdir('super-app');
+
+    fixturify.writeSync(tempDir, {
+      '.eslintrc.json': '{"extends": "eslint:recommended"}',
+      'test.js': `/* eslint-disable-next-line no-debugger */
+debugger
+console.log('test')`,
+      'package.json': `{
+  "devDependencies": {
+    "eslint": "^8.0.0"
+  }
+}
+`,
+    });
+
+    let result = await execa('npm', ['i'], { cwd: tempDir });
+
+    await ignoreAll(tempDir);
+
+    result = fixturify.readSync(tempDir);
+
+    expect(result['test.js']).to.equal(`/* eslint-disable no-undef */
+/* eslint-disable-next-line no-debugger */
+debugger
+console.log('test')`);
+  });
 });
